476501db8df91e5db5791067bb29f2657f0b345f
diegomduquec <diegomduquec@gmail.com>
2022-02-01 11:33:59 -0500

    Subiendo segunda parte del desafio de Digital House sobre eventos en JS del FrontEnd

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -7,7 +7,32 @@ window.onload = function(){
     article.classList.add('fondoTransparente');
     formulario.classList.add('fondoCRUD');
 
+    //EVENTOS
     titulo.addEventListener('mouseover', function () {
         titulo.style.color = '#0833a2'
+    })
+
+    let imputTitulo = document.querySelector('#titulo')
+    let estadoSecreto = 0;
+    imputTitulo.addEventListener('keyup', function (e) {
+        
+        if (e.key == 's') {
+            estadoSecreto = 1            
+        } else if (e.key == 'e' && estadoSecreto == 1) {
+            estadoSecreto = 2
+        } else if (e.key == 'c' && estadoSecreto == 2) {
+            estadoSecreto = 3
+        } else if (e.key == 'r' && estadoSecreto == 3) {
+            estadoSecreto = 4
+        } else if (e.key == 'e' && estadoSecreto == 4) {
+            estadoSecreto = 5
+        } else if (e.key == 't' && estadoSecreto == 5) {
+            estadoSecreto = 6
+        } else if (e.key == 'o' && estadoSecreto == 6) {
+            estadoSecreto = 0
+            alert('SECRETO MÁGICO')            
+        } else {
+            estadoSecreto = 0
+        }        
     })
 }
